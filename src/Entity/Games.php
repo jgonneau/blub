8ed9657f06a6fe76a89a31c44f2f338410a84304
Jean Paul Gonneau <jgonneau@j-p.work>
2020-03-11 11:23:46 +0100

    init symfprojet presley ImpulseApi

---
+++ src/Entity/Games.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use ApiPlatform\Core\Annotation\ApiResource;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ApiResource(
+ *      collectionOperations={"get"},
+ *      itemOperations={"get", "put"},
+ *      shortName="cheeses",
+ * 
+ *      normalizationContext={"groups"={"games_list:read"}},
+ *      denormalizationContext={"groups"={"games_list:write"}}
+ * )
+ * @ORM\Entity(repositoryClass="App\Repository\GamesRepository")
+ */
+class Games
+{
+    /**
+     * @ORM\Id()
+     * @ORM\GeneratedValue()
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $Title;
+
+    /**
+     * @ORM\Column(type="datetime")
+     */
+    private $createdAt;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitle(): ?string
+    {
+        return $this->Title;
+    }
+
+    public function setTitle(string $Title): self
+    {
+        $this->Title = $Title;
+
+        return $this;
+    }
+
+    public function getCreatedAt(): ?\DateTimeInterface
+    {
+        return $this->createdAt;
+    }
+
+    public function setCreatedAt(\DateTimeInterface $createdAt): self
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+}
